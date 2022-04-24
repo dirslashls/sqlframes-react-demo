b22864f3b5476593a80633840e2d3edbf14b93e1
Siva <siva.dirisala@gmail.com>
2022-04-23 21:40:24 -0700

    Removing dependency on @sqlframes/repl-app/react

--- src/js/App.js
+++ src/js/App.js
@@ -1,28 +1,33 @@
-import React, { Component, useRef, useEffect} from "react";
+import React, { Component, useRef, useEffect, createRef} from "react";
 import "./App.css";
 
 // this is an example to bundle SQL Frames directly into the app
 
 // Destructuring assignment of the exported namespaces 
-import { sqlframes, repl, repl_react } from '@sqlframes/repl-app/react';
+import { sqlframes, repl } from '@sqlframes/repl-app';
 // to ensure the css is also bundled into the application css bundle
 import '@sqlframes/repl-app/styles';
 
 // All the top level classes and objects exported in the namespaces
 const { DataFrame, SQL, Time, View } = sqlframes;
 const { REPL, REPLView } = repl;
-const { ReactREPL } = repl_react;
 
 // for autosuggest of the SQL Frames API within the REPL editor
 REPLView.addExtraLibs('/api/api.d.ts','api.d.ts');
 
 class App extends Component{
+	replRef = createRef();
 	render(){
 		return(
-			<div className="App">
-				<ReactREPL/>
+			<div ref={this.replRef} className="App">
 			</div>
 		);
+	}
+
+	componentDidMount() {
+		const _repl = new REPL();
+		_repl.scope = repl.Scope;
+		View.render(this.replRef.current,_repl);
 	}
 }
 

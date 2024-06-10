dirslashls/sqlframes-react-demo

commit ac93a7bd130818e95a5a89d12e0111834edd67c1

update to React 18 and latest sql frames

src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App, { ChartApp } from "@/js/App.tsx";
-ReactDOM.render(<App />, document.getElementById("root"));
-ReactDOM.render(<ChartApp />, document.getElementById("chart"));
+import { createRoot } from "react-dom/client";
+createRoot(document.getElementById("root")).render(<App/>);
+createRoot(document.getElementById("chart")).render(<ChartApp />);

config/webpack.common.js
@@ -26,7 +26,6 @@ function createTarget({ entry, outputType, template, file }) {
     // 'date-fns': { amd: 'date-fns', global: 'dateFns' }, // example of pre-bundling date-fns
     'papaparse': 'papaparse',
     'monaco-editor': { amd: 'monaco-editor', global: 'monaco' },
-    'htm': 'htm',
     'preact': 'preact',
     '@yaireo/tagify': {amd: '@yaireo/tagify', global: 'Tagify' },
     'interactjs': { amd: 'interactjs', global: 'interact' },

src/js/App.tsx
@@ -1,14 +1,12 @@
-import React, { Component, useRef, useEffect } from "react";
+import React, { Component, useMemo } from "react";
 import "./App.css";
 import { REPLReact } from "./REPLReact";
-import { DataFrameComponent } from "./DataFrameComponent";
 
 // this is an example to bundle SQL Frames directly into the app
 
 // Destructuring assignment of the exported namespaces 
-import { sqlframes, repl } from '@sqlframes/repl-app';
-// to ensure the css is also bundled into the application css bundle
-import '@sqlframes/repl-app/styles';
+import { sqlframes } from '@sqlframes/repl-app';
+import SQLFramesComponent from "./SQLFramesComponent";
 
 // All the top level classes and objects exported in the namespaces
 const { DataFrame, SQL, Time, View } = sqlframes;
@@ -20,25 +18,20 @@ class App extends Component{
 }
 
 const ChartApp = () => {
-	const chartRef = useRef(null);
-	const df = DataFrame.fromURL('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
-	const { groupBy, where: { eq }, agg: { count } } = SQL;
-	const chart = () => {
+	const [df,chart] = useMemo(() => {
+		const df = DataFrame.fromURL('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
+		const { groupBy, where: { eq }, agg: { count } } = SQL;
 		const gdf = df.gdf(groupBy('type'));
-		return gdf.chart.bar('type','Count')
+		return [df,gdf.chart.bar('type','Count')
 					.resize(600,400)
-					.svg();
-	};
-
-	useEffect(() =>{
-		View.render(chartRef.current,chart());
-	});
+					.svg()];
+	},[]);
 
 	return (
-		<div>
-			<div ref={chartRef}></div>
-			<DataFrameComponent df={df}/>
-		</div>
+		<>
+			<SQLFramesComponent value={chart}/>
+			<SQLFramesComponent value={df}/>
+		</>
 	)
 }
 

src/js/DataFrameComponent.tsx
@@ -1,22 +1,0 @@
-import React, { useRef, useEffect } from 'react';
-// Destructuring assignment of the exported namespaces 
-import { sqlframes } from '@sqlframes/repl-app';
-// to ensure the css is also bundled into the application css bundle
-import '@sqlframes/repl-app/styles';
-
-// All the top level classes and objects exported in the namespaces
-const { View } = sqlframes;
-
-const DataFrameComponent = function({ df }) {
-	const ref = useRef(null);
-
-	useEffect(() => {
-		View.render(ref.current,df);
-	});
-
-	return (
-		<div ref={ref}></div>
-	)
-}
-
-export { DataFrameComponent };

src/js/REPLReact.tsx
@@ -1,32 +1,20 @@
-import React, { Component, createRef} from "react";
+import React, { useMemo} from "react";
 
 // this is an example to bundle SQL Frames directly into the app
-
-// Destructuring assignment of the exported namespaces 
-import { sqlframes, repl } from '@sqlframes/repl-app';
-// to ensure the css is also bundled into the application css bundle
-import '@sqlframes/repl-app/styles';
+import * as sf from '@sqlframes/repl-app';
+import SQLFramesComponent from "./SQLFramesComponent";
 
 // All the top level classes and objects exported in the namespaces
-const { DataFrame, SQL, Time, View } = sqlframes;
-const { REPL, REPLView } = repl;
-
+const { REPLView } = sf.repl;
 // for autosuggest of the SQL Frames API within the REPL editor
 REPLView.addExtraLibs('/api/api.d.ts','api.d.ts');
 
-class REPLReact extends Component{
-	replRef = createRef<HTMLDivElement>();
-	render(){
-		return(
-			<div ref={this.replRef} className="App">
-			</div>
-		);
-	}
-
-	componentDidMount() {
-		const _repl = new REPL();
-		View.render(this.replRef.current,_repl);
-	}
+function REPLReact() {
+	const repl = useMemo(() => {
+		return new sf.repl.REPL();
+	},[]);
+	return <SQLFramesComponent value={repl}/>;
 }
+
 
 export { REPLReact };

src/js/SQLFramesComponent.js
@@ -1,0 +1,24 @@
+import React, { useRef, useEffect, useLayoutEffect } from 'react';
+import * as sf from '@sqlframes/repl-app';
+// to ensure the css is also bundled into the application css bundle
+import '@sqlframes/repl-app/styles';
+
+function SQLFramesComponent({ value }) {
+	const ref = useRef(null);
+	const { View } = sf.sqlframes;
+
+	useLayoutEffect(() => {
+		View.render(ref.current,value);
+	},[value]);
+
+	useEffect(() => {
+		const e = ref.current;
+		return () => { View.render(e,null); };
+	},[]);
+
+	return (
+		<div className="sf" ref={ref}></div>
+	)
+}
+
+export default SQLFramesComponent;

src/js/Wrapper.tsx
@@ -6,14 +6,18 @@ async function loadDependencies() {
 	const require = globalThis.require as any;
 	const define = globalThis.define as any;
 	require.config({ paths: libs });
-	define('monaco-editor',['vs/editor/editor.main'],(m) => m);
+	define('path',() => {});
+	define('fs',() => {});
+	define('monaco-editor',['vs/editor/editor.main'],(monaco) => {
+		monaco.editor.setTheme('vs-dark');
+		return monaco;
+	});
 	// dynamic modlules - these are defined as undefined if not already present so they are loaded ondemand
 	define('exceljs',() => globalThis.exceljs ?? void 0);
 	define('lunr',() => globalThis.lunr ?? void 0);
 	define('cytoscape',() => globalThis.cytoscape ?? void 0);
-	define('sqlframes',['htm','preact','echarts','exceljs','monaco-editor','lunr','@yaireo/tagify','acorn','interactjs','dompurify','eta','papaparse','highlightjs','cytoscape'],
-						(htm,preact,echarts,exceljs,monaco,lunr,Tagify,acorn,interact,DOMPurify,Eta,papaparse,highlightjs,cytoscape) => {
-		globalThis.htm = htm;
+	define('sqlframes',['preact','echarts','exceljs','monaco-editor','lunr','@yaireo/tagify','acorn','interactjs','dompurify','eta','papaparse','highlightjs','cytoscape'],
+						(preact,echarts,exceljs,monaco,lunr,Tagify,acorn,interact,DOMPurify,Eta,papaparse,highlightjs,cytoscape) => {
 		globalThis.preact = preact;
 		globalThis.echarts = echarts;
 		globalThis.ExcelJS = exceljs;
@@ -55,7 +59,7 @@ export default loadable(async () => {
 	console.debug('loading');
 	await loadDependencies();
 	// @ts-ignore
-	const { REPLReact } = await import('./REPLREact');
+	const { REPLReact } = await import('./REPLReact');
 	return REPLReact;
 },{
 	fallback: <div style={{height:'5em'}}><div className="loader">Loading...</div></div>

src/template.html
@@ -14,11 +14,8 @@
 		import {esmLibs} from '/js/sqlframes/libs.mjs';
 		esmLibs['date-fns'] = undefined;
 		import sfloader from '/js/sqlframes/main.mjs';
-		define('monaco-editor',['vs/editor/editor.main'],(monaco) => {
-			monaco.editor.setTheme('vs-dark');
-			return monaco;
-		});
 		await sfloader.setupDependencies();
+		require = requirejs.config({ context: 'sf' });
 	</script>
 	<title><%= htmlWebpackPlugin.options.title %></title>
 	</head>

commit cd59f8818ce5875a5b7da53a7627ae776abaa22a

Fixing compilation error introduced in ac93a7b

src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App, { ChartApp } from "@/js/App.tsx";
-import { createRoot } from "react-dom/client";
 createRoot(document.getElementById("root")).render(<App/>);
 createRoot(document.getElementById("chart")).render(<ChartApp />);
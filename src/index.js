cd59f8818ce5875a5b7da53a7627ae776abaa22a
dirslashls <siva.dirisala@gmail.com>
2024-06-10 12:12:24 -0700

    Fixing compilation error introduced in ac93a7b

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App, { ChartApp } from "@/js/App.tsx";
-import { createRoot } from "react-dom/client";
 createRoot(document.getElementById("root")).render(<App/>);
 createRoot(document.getElementById("chart")).render(<ChartApp />);
